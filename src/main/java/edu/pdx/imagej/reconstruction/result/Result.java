234f89941196cc789b9b18cc85f2dc8423d0b694
David Cohoe <sudgylacmoe@gmail.com>
2019-05-14 11:28:02 -0700

    Added Result plugin

---
+++ src/main/java/edu/pdx/imagej/reconstruction/result/Result.java
@@ -0,0 +1,244 @@
+/* Copyright (C) 2019 Portland State University
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of version 3 of the GNU Lesser General Public License
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * For any questions regarding the license, please contact the Free Software
+ * Foundation.  For any other questions regarding this program, please contact
+ * David Cohoe at dcohoe@pdx.edu.
+ */
+
+package edu.pdx.imagej.reconstruction.result;
+
+import java.io.File;
+import java.nio.file.Paths;
+import java.util.AbstractList;
+
+import ij.IJ;
+import ij.ImagePlus;
+import ij.ImageStack;
+import ij.measure.Calibration;
+import ij.process.ImageProcessor;
+import ij.process.FloatProcessor;
+
+import org.scijava.Priority;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+import org.scijava.ui.UIService;
+
+import edu.pdx.imagej.dynamic_parameters.DParameter;
+import edu.pdx.imagej.reconstruction.ReconstructionPlugin;
+import edu.pdx.imagej.reconstruction.AbstractReconstructionPlugin;
+import edu.pdx.imagej.reconstruction.ReconstructionField;
+import edu.pdx.imagej.reconstruction.UnitService;
+
+@Plugin(type = ReconstructionPlugin.class, priority = Priority.LAST)
+public class Result extends AbstractReconstructionPlugin {
+    @Parameter UIService P_ui;
+    @Parameter UnitService P_units;
+
+    @Override
+    public DParameter param() {return M_param;}
+    @Override
+    public void process_hologram_param(ImagePlus hologram)
+    {
+        M_hologram = hologram;
+        M_pixel_width = hologram.getProcessor().getWidth();
+        M_pixel_height = hologram.getProcessor().getHeight();
+    }
+    @Override
+    public void process_dimensions_param(double width, double height)
+    {
+        M_cal = new Calibration();
+        M_cal.pixelWidth = width / M_pixel_width;
+        M_cal.pixelHeight = height / M_pixel_height;
+        M_cal.setUnit(P_units.image().toString());
+    }
+    @Override
+    public void process_ts_param(AbstractList<Integer> ts)
+    {
+        M_t_size = ts.size();
+    }
+    @Override
+    public void process_zs_param(AbstractList<Double> zs)
+    {
+        M_z_size = zs.size();
+        if (M_param.get_value().save_to_file) {
+            try {
+                for (double z : zs) {
+                    if (M_param.get_value().amplitude) {
+                        new File(Paths.get(M_param.get_value().save_directory,
+                            "Amplitude", format_z(z)).toString()).mkdirs();
+                    }
+                    if (M_param.get_value().phase) {
+                        new File(Paths.get(M_param.get_value().save_directory,
+                            "Phase", format_z(z)).toString()).mkdirs();
+                    }
+                    if (M_param.get_value().real) {
+                        new File(Paths.get(M_param.get_value().save_directory,
+                            "Real", format_z(z)).toString()).mkdirs();
+                    }
+                    if (M_param.get_value().imaginary) {
+                        new File(Paths.get(M_param.get_value().save_directory,
+                            "Imaginary", format_z(z)).toString()).mkdirs();
+                    }
+                }
+            }
+            catch (SecurityException e) {
+                P_ui.showDialog("Unable to create directories: "
+                    + e.getMessage(), "Error");
+                M_error = true;
+                return;
+            }
+        }
+    }
+    @Override
+    public void process_beginning()
+    {
+        if (!M_param.get_value().save_to_file) {
+            if (M_param.get_value().amplitude) {
+                M_amplitude = new ImageStack(M_pixel_width, M_pixel_height);
+            }
+            if (M_param.get_value().phase) {
+                M_phase = new ImageStack(M_pixel_width, M_pixel_height);
+            }
+            if (M_param.get_value().real) {
+                M_real = new ImageStack(M_pixel_width, M_pixel_height);
+            }
+            if (M_param.get_value().imaginary) {
+                M_imaginary = new ImageStack(M_pixel_width, M_pixel_height);
+            }
+        }
+    }
+    @Override
+    public void process_propagated_field(ReconstructionField field, int t,
+                                         double z)
+    {
+        if (M_param.get_value().amplitude) {
+            process_particular(field.field().get_amp(), t, z, M_amplitude,
+                               "Amplitude", get_slice_label(t));
+        }
+        if (M_param.get_value().phase) {
+            process_particular(field.field().get_arg(), t, z, M_phase,
+                               "Phase", get_slice_label(t));
+        }
+        if (M_param.get_value().real) {
+            process_particular(field.field().get_real(), t, z, M_real,
+                               "Real", get_slice_label(t));
+        }
+        if (M_param.get_value().imaginary) {
+            process_particular(field.field().get_imag(), t, z, M_imaginary,
+                               "Imaginary", get_slice_label(t));
+        }
+    }
+    private void process_particular(double[][] d_result, int t, double z,
+                                    ImageStack stack, String type, String label)
+    {
+        float[][] result = new float[d_result.length][d_result[0].length];
+        for (int x = 0; x < result.length; ++x) {
+            for (int y = 0; y < result[0].length; ++y) {
+                result[x][y] = (float)d_result[x][y];
+            }
+        }
+        ImageProcessor proc = new FloatProcessor(result);
+        if (M_param.get_value().type == ResultOptions.Type.Type8Bit) {
+            proc = proc.convertToByteProcessor();
+        }
+        else if (M_param.get_value().type == ResultOptions.Type.Type16Bit) {
+            proc = proc.convertToShortProcessor();
+        }
+
+        if (M_param.get_value().save_to_file) {
+            ImagePlus temp_img = new ImagePlus("", proc);
+            temp_img.setCalibration(M_cal);
+            IJ.saveAsTiff(temp_img, Paths.get(
+                M_param.get_value().save_directory, type, format_z(z),
+                format_t(t)).toString());
+            temp_img.close();
+        }
+        // Not save to file
+        else {
+            stack.addSlice(label + ", z = " + format_z(z), proc);
+        }
+    }
+    @Override
+    public void process_ending()
+    {
+        almost_process_ending();
+        if (!M_param.get_value().save_to_file) {
+            if (M_param.get_value().amplitude) M_amplitude_imp.show();
+            if (M_param.get_value().phase) M_phase_imp.show();
+            if (M_param.get_value().real) M_real_imp.show();
+            if (M_param.get_value().imaginary) M_imaginary_imp.show();
+        }
+    }
+    void almost_process_ending() // Package private for testing
+    {
+        if (!M_param.get_value().save_to_file) {
+            if (M_param.get_value().amplitude) {
+                M_amplitude_imp = create_imp(M_amplitude, "Amplitude");
+            }
+            if (M_param.get_value().phase) {
+                M_phase_imp = create_imp(M_phase, "Phase");
+            }
+            if (M_param.get_value().real) {
+                M_real_imp = create_imp(M_real, "Real");
+            }
+            if (M_param.get_value().imaginary) {
+                M_imaginary_imp = create_imp(M_imaginary, "Imaginary");
+            }
+        }
+    }
+    private ImagePlus create_imp(ImageStack stack, String label)
+    {
+        int bit_depth = 8;
+        if (M_param.get_value().type == ResultOptions.Type.Type16Bit) {
+            bit_depth = 16;
+        }
+        else if (M_param.get_value().type == ResultOptions.Type.Type32Bit) {
+            bit_depth = 32;
+        }
+        ImagePlus imp = IJ.createHyperStack(label, M_pixel_width,
+                                            M_pixel_height, 1, M_z_size,
+                                            M_t_size, bit_depth);
+        imp.setStack(stack);
+        imp.setCalibration(M_cal);
+        return imp;
+    }
+    @Override public boolean has_error() {return M_error;}
+
+    private String format_z(double z) {return String.format("%.3f", z);}
+    private String format_t(int t) {return Integer.toString(t);}
+    private String get_slice_label(int t)
+    {
+        String result = M_hologram.getImageStack().getSliceLabel(t);
+        if (result == null) result = M_hologram.getTitle();
+        return result;
+    }
+
+    private ResultParameter M_param = new ResultParameter();
+    private ImageStack M_amplitude;
+    private ImageStack M_phase;
+    private ImageStack M_real;
+    private ImageStack M_imaginary;
+    ImagePlus M_amplitude_imp; // Package private to allow testing
+    ImagePlus M_phase_imp;
+    ImagePlus M_real_imp;
+    ImagePlus M_imaginary_imp;
+    private boolean M_error = false;
+    private ImagePlus M_hologram;
+    private Calibration M_cal;
+    private int M_z_size;
+    private int M_t_size;
+    private int M_pixel_width;
+    private int M_pixel_height;
+}
