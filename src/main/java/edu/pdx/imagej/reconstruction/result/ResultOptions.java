412795e8ae3b03bf685b1debece5092b33654748
David Cohoe <sudgylacmoe@gmail.com>
2019-05-14 11:27:14 -0700

    Added ResultOptions

---
+++ src/main/java/edu/pdx/imagej/reconstruction/result/ResultOptions.java
@@ -0,0 +1,31 @@
+/* Copyright (C) 2019 Portland State University
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of version 3 of the GNU Lesser General Public License
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * For any questions regarding the license, please contact the Free Software
+ * Foundation.  For any other questions regarding this program, please contact
+ * David Cohoe at dcohoe@pdx.edu.
+ */
+
+package edu.pdx.imagej.reconstruction.result;
+
+public class ResultOptions {
+    public boolean amplitude;
+    public boolean phase;
+    public boolean real;
+    public boolean imaginary;
+    public enum Type {Type8Bit, Type16Bit, Type32Bit};
+    public Type type;
+    public boolean save_to_file;
+    public String save_directory;
+}
