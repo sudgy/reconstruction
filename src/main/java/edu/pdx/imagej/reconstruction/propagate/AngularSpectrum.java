4faad3c7d3bf79a73ca1765d11cf5fbae41d2828
David Cohoe <sudgylacmoe@gmail.com>
2019-05-15 15:29:23 -0700

    Added AngularSpectrum class, does nothing yet

---
+++ src/main/java/edu/pdx/imagej/reconstruction/propagate/AngularSpectrum.java
@@ -0,0 +1,36 @@
+/* Copyright (C) 2019 Portland State University
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of version 3 of the GNU Lesser General Public License
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * For any questions regarding the license, please contact the Free Software
+ * Foundation.  For any other questions regarding this program, please contact
+ * David Cohoe at dcohoe@pdx.edu.
+ */
+
+package edu.pdx.imagej.reconstruction.propagation;
+
+import org.scijava.Priority;
+import org.scijava.plugin.Plugin;
+
+import edu.pdx.imagej.reconstruction.ReconstructionField;
+
+@Plugin(type = PropagationPlugin.class,
+        name = "Angular Spectrum",
+        priority = Priority.VERY_HIGH)
+public class AngularSpectrum extends AbstractPropagationPlugin {
+    @Override
+    public void propagate(ReconstructionField field, double z)
+    {
+
+    }
+}
