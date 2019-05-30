3558aaa286bfaad13c016821a7340ecf5a6aaafa
David Cohoe <sudgylacmoe@gmail.com>
2019-05-30 10:52:33 -0700

    Added an almost silly test for None

---
+++ src/test/java/edu/pdx/imagej/reconstruction/reference/NoneTest.java
@@ -0,0 +1,32 @@
+/* Copyright (C) 2019 Portland State University
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of version 3 of the GNU Lesser General Public License
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * For any questions regarding the license, please contact the Free Software
+ * Foundation.  For any other questions regarding this program, please contact
+ * David Cohoe at dcohoe@pdx.edu.
+ */
+
+package edu.pdx.imagej.reconstruction.reference;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+public class NoneTest {
+    @Test public void test_null()
+    {
+        None test = new None();
+        assertTrue(test.get_reference_holo(null, 0) == null);
+    }
+}
