71ec489aed9797b7eb85b95bac14fd09eed01a21
David Cohoe <sudgylacmoe@gmail.com>
2019-06-14 10:13:52 -0700

    Added package info for filter package

---
+++ src/main/java/edu/pdx/imagej/reconstruction/filter/package-info.java
@@ -0,0 +1,5 @@
+/** This plugin gets the filter from the user and then applies it to all of the
+ * holograms while processing.  Many other plugins use this plugin as well so
+ * that they can use the same filter that is being used everywhere.
+ */
+package edu.pdx.imagej.reconstruction.filter;
