661e2d6dffb5dbb14940c210ff51c4a39a6d014d
David Cohoe <sudgylacmoe@gmail.com>
2019-06-14 10:43:25 -0700

    Made package info for units package

---
+++ src/main/java/edu/pdx/imagej/reconstruction/units/package-info.java
@@ -0,0 +1,6 @@
+/** This package deals with length units used throughout the package.  Any
+ * physical value is passed around everywhere as a {@link DistanceUnitValue},
+ * which will automatically convert to the units that you wish when converting
+ * to a <code>double</code>.
+ */
+package edu.pdx.imagej.reconstruction.units;
