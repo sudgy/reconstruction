8c3fca3078b9741f3e6a587ff9b660915f44c947
David Cohoe <sudgylacmoe@gmail.com>
2019-06-14 10:35:10 -0700

    Made package info for Propagation package

---
+++ src/main/java/edu/pdx/imagej/reconstruction/propagation/package-info.java
@@ -0,0 +1,5 @@
+/** This plugin performs numerical propagation on the filtered fields.
+ * Currently, only {@link AngularSpectrum} is supported, but you may make your
+ * own by making a {@link PropagationPlugin}.
+ */
+package edu.pdx.imagej.reconstruction.propagation;
