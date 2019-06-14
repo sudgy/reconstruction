2d5a50984800e37adcec1e47b5f8c1d2d290818c
David Cohoe <sudgylacmoe@gmail.com>
2019-06-14 10:13:22 -0700

    Added package info for top-level package

---
+++ src/main/java/edu/pdx/imagej/reconstruction/package-info.java
@@ -0,0 +1,6 @@
+/** This package holds all of the basic pieces of reconstruction, including the
+ * command and the more mathematical pieces used to calculate things.  If you
+ * are making your own plugin, the {@link ComplexField} and
+ * {@link ReconstructionField} classes are the main things you would use here.
+ */
+package edu.pdx.imagej.reconstruction;
