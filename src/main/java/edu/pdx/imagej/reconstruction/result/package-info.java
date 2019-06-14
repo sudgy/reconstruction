40cb78497c3a0f8b28829c3c4e11b72c1d2c1d3e
David Cohoe <sudgylacmoe@gmail.com>
2019-06-14 10:39:13 -0700

    Added two tiny package infos to result and status

---
+++ src/main/java/edu/pdx/imagej/reconstruction/result/package-info.java
@@ -0,0 +1,5 @@
+/** This plugin displays the result of the command to the user either through
+ * displaying in an ImagePlus or saving to a file.  I honestly can't think of
+ * any reason you would care about this.
+ */
+package edu.pdx.imagej.reconstruction.result;
---
+++ src/main/java/edu/pdx/imagej/reconstruction/status/package-info.java
@@ -0,0 +1,4 @@
+/** This plugin displays progress/status of the command.  I honestly can't
+ * think of any reason you would care about this.
+ */
+package edu.pdx.imagej.reconstruction.status;
