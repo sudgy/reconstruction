1bfefd8260630d4022563e48a896bc5cd294eba0
David Cohoe <sudgylacmoe@gmail.com>
2019-06-20 14:02:47 -0700

    Added a test for Self

---
+++ src/test/java/edu/pdx/imagej/reconstruction/reference/SelfTest.java
@@ -0,0 +1,33 @@
+/* Copyright (C) 2019 Portland State University
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of version 3 of the GNU Lesser General Public License
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * For any questions regarding the license, please contact the Free Software
+ * Foundation.  For any other questions regarding this program, please contact
+ * David Cohoe at dcohoe@pdx.edu.
+ */
+
+package edu.pdx.imagej.reconstruction.reference;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+public class SelfTest {
+    @Test public void test_dont()
+    {
+        Self test = new Self();
+        // You laugh, but this was a bug
+        assertTrue(test.dont_use_same_roi());
+    }
+}
