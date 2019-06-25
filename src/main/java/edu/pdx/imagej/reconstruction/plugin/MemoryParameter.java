78d75df7b2dcd8afe25fc5c969e78651f199d5f6
David Cohoe <sudgylacmoe@gmail.com>
2019-06-25 11:19:52 -0700

    Made MemoryParameter also ask about caching

--- src/main/java/edu/pdx/imagej/reconstruction/plugin/MemoryParameter.java
+++ src/main/java/edu/pdx/imagej/reconstruction/plugin/MemoryParameter.java
@@ -25,6 +25,7 @@ import ij.IJ;
 
 import org.scijava.plugin.Plugin;
 
+import edu.pdx.imagej.dynamic_parameters.BoolParameter;
 import edu.pdx.imagej.dynamic_parameters.DoubleParameter;
 import edu.pdx.imagej.dynamic_parameters.HoldingParameter;
 import edu.pdx.imagej.dynamic_parameters.IntParameter;
@@ -34,11 +35,13 @@ import edu.pdx.imagej.dynamic_parameters.RadioParameter;
  * has two ways to select the memory usage: A flat value, or a percentage of
  * ImageJ's max memory.
  */
-public class MemoryParameter extends HoldingParameter<Supplier<Integer>> {
-    public MemoryParameter(String label, boolean initial_percent,
-                           double percent_value, int flat_value)
+public class MemoryParameter extends HoldingParameter<Supplier<Long>> {
+    public MemoryParameter(String label, boolean do_cache,
+                           boolean initial_percent, double percent_value,
+                           int flat_value)
     {
         super(label);
+        M_do_cache = do_cache;
         M_initial_percent = initial_percent;
         M_percent_value = percent_value;
         M_flat_value = flat_value;
@@ -47,6 +50,8 @@ public class MemoryParameter extends HoldingParameter<Supplier<Integer>> {
     public void initialize()
     {
         String[] choices = {"Percent", "Flat"};
+        M_do = add_parameter(BoolParameter.class, "Cache partial results?",
+                             true);
         M_choice = new RadioParameter("Memory limit", choices,
                                       M_initial_percent ? "Percent" : "Flat",
                                       1, 2);
@@ -54,7 +59,7 @@ public class MemoryParameter extends HoldingParameter<Supplier<Integer>> {
         M_percent = add_parameter(DoubleParameter.class, M_percent_value,
                                   "Percent Value", "%");
         M_flat = add_parameter(IntParameter.class, M_flat_value, "Flat Value",
-                               "Bytes");
+                               "MB");
         M_percent.set_bounds(0.0, 100.0);
         M_flat.set_bounds(0, Integer.MAX_VALUE);
         set_visibilities();
@@ -67,31 +72,29 @@ public class MemoryParameter extends HoldingParameter<Supplier<Integer>> {
         set_visibilities();
         check_for_errors();
     }
+    // We trust that the programmer put in the right values for everything in
+    // the constructor
     @Override
-    public void read_from_prefs(Class<?> cls, String name)
-    {
-        super.read_from_prefs(cls, name);
-        set_visibilities();
-        check_for_errors();
-    }
+    public void read_from_prefs(Class<?> cls, String name) {}
     @Override
-    public Supplier<Integer> get_value()
+    public Supplier<Long> get_value()
     {
+        if (!M_do.get_value()) return null;
         if (M_choice.get_value().equals("Percent")) {
             final double percent = M_percent.get_value();
-            return new Supplier<Integer>() {
+            return new Supplier<Long>() {
                 @Override
-                public Integer get()
+                public Long get()
                 {
-                    return (int)(IJ.maxMemory() * percent * 0.01);
+                    return (long)(IJ.maxMemory() * percent * 0.01);
                 }
             };
         }
         else {
-            final int flat = M_flat.get_value();
-            return new Supplier<Integer>() {
+            final long flat = M_flat.get_value() * (1024L * 1024L);
+            return new Supplier<Long>() {
                 @Override
-                public Integer get()
+                public Long get()
                 {
                     return flat;
                 }
@@ -101,23 +104,34 @@ public class MemoryParameter extends HoldingParameter<Supplier<Integer>> {
 
     private void set_visibilities()
     {
-        boolean percent = M_choice.get_value().equals("Percent");
-        M_percent.set_new_visibility(percent);
-        M_flat.set_new_visibility(!percent);
+        if (M_do.get_value()) {
+            M_choice.set_new_visibility(true);
+            boolean percent = M_choice.get_value().equals("Percent");
+            M_percent.set_new_visibility(percent);
+            M_flat.set_new_visibility(!percent);
+        }
+        else {
+            M_choice.set_new_visibility(false);
+            M_percent.set_new_visibility(false);
+            M_flat.set_new_visibility(false);
+        }
     }
     private void check_for_errors()
     {
-        if (!M_choice.get_value().equals("Percent")
-                && M_flat.get_value() > IJ.maxMemory()) {
-            set_warning("Flat value " + M_flat.get_value() + " is greater than "
-                + "ImageJ's maximum allowed memory, " + IJ.maxMemory() +".");
+        int flat_mb = M_flat.get_value();
+        int ij_mb = (int)(IJ.maxMemory() / (1024*1024));
+        if (!M_choice.get_value().equals("Percent") && flat_mb > ij_mb) {
+            set_warning("Warning: Flat value " + flat_mb + " is greater than "
+                + "ImageJ's maximum allowed memory, " + ij_mb + ".");
         }
         else set_warning(null);
     }
 
+    private boolean M_do_cache;
     private boolean M_initial_percent;
     private double M_percent_value;
     private int M_flat_value;
+    private BoolParameter M_do;
     private RadioParameter M_choice;
     private DoubleParameter M_percent;
     private IntParameter M_flat;
